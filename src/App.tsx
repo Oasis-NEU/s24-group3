087f5cebbd74cd0d708e1f9c1e890ba5ad5dd5b1
gpreston22 <preston.gra@northeastern.edu>
2024-02-25 11:01:58 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 export default function App() {
+  <head> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap" /> </head>
   return <h1>Hello!
     <h2>🐡 🐠 🐟 🐬 This is maya and grace 🐳 🐋 🦈 🐙 🦑</h2>
     <section id="Task-Section">
