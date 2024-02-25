58840e79a44fb5d51db4c2a3140297db390ad039
gpreston22 <preston.gra@northeastern.edu>
2024-02-25 11:00:13 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './App.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+function ExampleComponent() {
+  return (
+    <div className="my-app">
+      This text uses Roboto font.
+    </div>
+  );
+}
+
+export default ExampleComponent;
