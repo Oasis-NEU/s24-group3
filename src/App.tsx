8a59fe939e0184b92c5bb72d851f6c3970419d4b
mayakarintholil <mayakarintholil@gmail.com>
2024-02-25 10:32:46 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export default function App() {
-  return <h1>Hello!</h1>;
+  return <h1>Hello!
+    <h2>My name is Maya</h2>
+    <section id="Task-Section">
+      <div>
+        <p>This is our underwater page.</p>
+      </div>
+
+    </section>
+
+  </h1>;
+ 
 }
