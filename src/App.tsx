bd8dee0c379f7bb25b779986be6b13ea4688c698
gpreston22 <preston.gra@northeastern.edu>
2024-02-25 10:50:40 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 export default function App() {
   return <h1>Hello!
-    <h2>My name is Maya</h2>
+    <h2>🐡 🐠 🐟 🐬 This is maya and grace 🐳 🐋 🦈 🐙 🦑</h2>
     <section id="Task-Section">
       <div>
-        <p>This is our underwater page.</p>
+        <p>This is our underwater page. </p>
       </div>
 
+    </section>
+
+    <section id="Bullet list">
+      <div>
+        <p> 🐠 These are your tasks for the week 🐠</p>
+      </div>
     </section>
 
   </h1>;
